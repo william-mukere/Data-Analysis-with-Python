8deeb859f69101ada3b3c8ea650465d9c87f06d7
William Mukere <74424606+william-mukere@users.noreply.github.com>
2023-08-02 06:14:29 +0000

    creating a program for grading score for highschool students in Kenya

---
+++ grading.py
@@ -0,0 +1,13 @@
+score = int(input("what is the score ? "))
+
+if  (score >= 70):
+    print("A")
+elif (score >= 60):
+    print("B")
+elif  (score >= 50):
+    print("C")
+elif  (score >= 40):
+    print("D")
+else:
+    print("E")
+
