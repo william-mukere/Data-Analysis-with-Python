10dd5f4755671c1780ab4ced11f6110ccec915a0
William Mukere <74424606+william-mukere@users.noreply.github.com>
2023-08-02 12:18:55 +0000

    creating a program that check house name from given name

--- house.py
+++ house.py
@@ -1,11 +1,7 @@
 name = input("what's your name ? ")
 
 match name:
-    case "Harry":
-        print("Gryffindor")
-    case "Hermione":
-        print("Gryffindor")
-    case "Ron":
+    case "Harry" | "Hermione" | "Ron":
         print("Gryffindor")
     case "Draco":
         print ("Slytherin")
